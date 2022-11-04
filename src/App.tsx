28131d9eff7152095071ce5569db5cf9ddc12c2b
max <max>
2022-11-05 02:32:27 +1000

    app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Container from '@mui/material/Container';
+import { LocalizationProvider } from "@mui/x-date-pickers";
+
+import { CurrenciesForm } from './features/currencies-form';
+import { RateChart } from './features/rate-chart';
+import { Notifications } from './features/notifications';
+import { Preloader } from './features/preloaders';
+
+export function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Container sx={{ paddingTop: '30px' }}>
+          <CurrenciesForm />
+          <RateChart />
+        </Container>
+
+        <Notifications />
+      </LocalizationProvider>
+
+      <Preloader />
+    </>
   );
 }
-
-export default App;
