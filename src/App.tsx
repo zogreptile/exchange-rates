c663aabb68acb40d73917376daff26077368bfc8
max <max>
2022-11-05 02:34:46 +1000

    changed global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import Container from '@mui/material/Container';
 import { LocalizationProvider } from "@mui/x-date-pickers";
+import { ThemeProvider } from '@mui/material/styles';
 
 import { CurrenciesForm } from './features/currencies-form';
 import { RateChart } from './features/rate-chart';
 import { Notifications } from './features/notifications';
 import { Preloader } from './features/preloaders';
 
+import { theme } from './common/css-theme';
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <Container sx={{ paddingTop: '30px' }}>
           <CurrenciesForm />
@@ -20,6 +23,6 @@ export function App() {
       </LocalizationProvider>
 
       <Preloader />
-    </>
+    </ThemeProvider>
   );
 }
